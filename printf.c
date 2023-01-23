91940ae2b871a629854505d3202c33ca221ed991
SaraaMaher <saraamaher13@gmail.com>
2023-01-23 05:35:26 -0800

    0

--- printf.c
+++ printf.c
@@ -8,7 +8,7 @@ int _printf(const char *format, ...)
 {
 int i = 0, j, count = 0;
 va_list arg;
-char c[] = {{'c', parse_char}, {'s', parse_str}, {'%', parse_perc}};
+type_t c[] = {{'c', parse_char}, {'s', parse_str}, {'%', parse_perc}};
 if (!format)
 return (-1);
 va_start(arg, format);
